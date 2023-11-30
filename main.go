ccf6819578aa166899ec6e47d32ec321ce3656bb
Satoki Hata <satokihata@gmail.com>
2023-12-01 08:54:00 +0900

    【add】main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func main() {
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("connect to DB")
+	}
+}
