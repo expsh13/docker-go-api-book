8312e90d5212c293e32afd596c6037269c763864
Satoki Hata <satokihata@gmail.com>
2023-12-03 11:15:16 +0900

    【update】データベース接続

--- main.go
+++ main.go
@@ -3,18 +3,25 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// データベースへの接続情報を宣言する
 	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+
+	// データベースに接続
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer db.Close()
+
+	// データベースへの疎通確認
 	if err := db.Ping(); err != nil {
 		fmt.Println(err)
 	} else {
