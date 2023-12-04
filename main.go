9583fd5f72c3d20387e840200856bd2338d0bbda
Satoki Hata <satokihata@gmail.com>
2023-12-04 12:37:34 +0900

    【add】データの取得処理

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"dbsample/models"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -21,10 +22,28 @@ func main() {
 	}
 	defer db.Close()
 
-	// データベースへの疎通確認
-	if err := db.Ping(); err != nil {
+	const sqlStr = `select title, contents, username, nice from articles;`
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	// rows に存在するレコードそれぞれに対して、繰り返し処理を実行する
+	for rows.Next() {
+		// 変数 article の各フィールドに、取得レコードのデータを入れる
+		// (SQL クエリの select 句から、タイトル・本文・ユーザー名・いいね数が返ってくることはわかっている)
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName,
+			&article.NiceNum)
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			articleArray = append(articleArray, article)
+		}
+	}
+	fmt.Printf("%+v\n", articleArray)
 }
